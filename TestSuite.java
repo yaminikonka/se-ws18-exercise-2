87fe6b61c952c96439f6a9ffcf40000efd56116e
Florian Echtler <floe@butterbrot.org>
2018-10-11 14:24:17 +0200

    update testsuite

--- TestSuite.java
+++ TestSuite.java
@@ -5,6 +5,11 @@ public class TestSuite {
 
   @Test public static void ImageTest1() {
     Image i = new Image(100,100);
+    assert (i.data.length == 30000);
+  }
+
+  @Test public static void ImageTest2() {
+    Image i = new Image(100,100);
     i.set(0,0,0x123456);
     assert (i.data[0] == (byte)0x12);
     assert (i.data[1] == (byte)0x34);
@@ -12,9 +17,20 @@ public class TestSuite {
     assert (i.data[3] == (byte)0x00);
   }
 
-  @Test public static void ImageTest2() {
+  @Test public static void ImageTest3() {
     Image i = new Image(100,100);
+    i.set(99,99,0x123456);
+    int len = i.data.length;
+    assert (false);
+  }
 
+  @Test public static void ImageTest4() throws java.io.FileNotFoundException,java.io.IOException {
+    String filename = "test.ppm";
+    Image i = new Image(100,100);
+    i.write(filename);
+    java.io.File f = new java.io.File(filename);
+    boolean exists = f.exists() && f.isFile();
+    assert (exists);
   }
 
 }
